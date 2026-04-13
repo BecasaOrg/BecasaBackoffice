9895c2deee34be9310a700e6057dc29c9198bc2e
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-04-13 14:54:43 -0500

    fix/ proxy

--- proxy.ts
+++ proxy.ts
@@ -2,19 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function proxy(request: NextRequest) {
-    const cookieStore = await cookies();
-    const token = cookieStore.get("auth_token");
+  const cookieStore = await cookies();
+  const token = cookieStore.get("auth_token");
 
-    if (!token) {
-        return NextResponse.json(
-            { message: "No autenticado" },
-            { status: 401 }
-        );
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: "/dashboard",
+  matcher: "/dashboard/:path*",
 };
